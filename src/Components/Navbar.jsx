aaf0268b8142bdeb3bddca624733d009566cc6ee
▄︻デS̷a̷n̷s̷k̷a̷r̷ ̷N̷a̷n̷d̷a̷══━一 <243898646+temporary-uses@users.noreply.github.com>
2025-11-16 18:47:38 +0530

    Feat: Render Navbar links dynamically using props
    Refactors src/Components/Navbar.jsx to receive navigation data via props and render links dynamically.
    
    * Updates the `Navbar` component signature to destructure the `links` prop.
    * Replaces the hardcoded list of `<li>` elements with a map function (`links.map(...)`) to iterate and render links based on the data passed from the parent component (App.jsx).
    
    This makes the Navbar component reusable and easily configurable.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import './Navbar.css'
 
 
-const Navbar = () => {
+const Navbar = ({ links }) => {
   return (
     <div>
       <nav className='navbar-with-logo'>
         <div className="logohead"> My Portfolio </div>
         <ul className='nav-links'>
-            <li><a href="http://">Home</a></li>
+          {links.map((links, index) => (
+            <li key={index}><a href="#">{links.name}</a></li>
+          ))}
+          {/* <li><a href="http://">Home</a></li>
             <li><a href="http://">About</a></li>
             <li><a href="http://">Contact</a></li>
-            <li><a href="http://">Resume</a></li>
+            <li><a href="http://">Resume</a></li> */}
         </ul>
       </nav>
     </div>
