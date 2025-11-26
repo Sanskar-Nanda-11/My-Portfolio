25a848a354f51a926bfb86a18d7f5440bf416f06
▄︻デS̷a̷n̷s̷k̷a̷r̷ ̷N̷a̷n̷d̷a̷══━一 <88901390+Sanskar-Nanda-11@users.noreply.github.com>
2025-11-26 07:11:32 +0530

    Feat: Implement Hero component structure with decorative background
    Adds the structural and stylistic foundation for the Hero component.
    
    * **Layout:** The component is designed as a relative container with a minimum height for large initial viewing.
    * **Styling:** Implements complex Tailwind CSS utility classes to create a large, absolutely positioned, rotated, and highly colored gradient shape within the component. This shape serves as a decorative background element for the main hero content.

---
+++ src/components/hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='relative overflow-hidden min-h[550px] sm:min-h-[600px] flex flex-col items-center'>
+        <div className='md-h-[550px] h-[550px] w-[450px] 
+        /* bg-gradient-to-r */
+        bg-linear-to-r absolute from-[#6D2897] via-[#Be6cf5] to-[#bb61c5 transform rotate-45 z-0 right-2 top-28 rounded-2xl'></div>
+      
+    </div>
+  )
+}
+
+export default Hero
